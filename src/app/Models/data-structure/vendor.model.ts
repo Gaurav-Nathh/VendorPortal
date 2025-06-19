3de49a3b677060ba15528b533a4b6af618415974
ayush <panwarkanu2001@gmail.com>
2025-06-19 23:31:39 +0530

    po and ip

--- src/app/Models/data-structure/vendor.model.ts
+++ src/app/Models/data-structure/vendor.model.ts
@@ -17,3 +17,54 @@ export class VendorFormData {
     status: 'draft' | 'confirm' = 'draft'; 
   }
   
+
+
+  export class PoFormModel {
+  
+}
+
+export class SearchVendorModel {
+  Search: string = "";
+  EmpId: number | null = null;
+  ActIds: number[] = [2, 1, 3];
+  BtpId: number = 0;
+  SearchType: string = "";
+  SearchColumn?: string; 
+  AcgGroups?: string;    
+  AcgCategorys: string = "";
+  MaxRecord: number = 100;
+  AcgIds: string = "";
+}
+
+export class TDSDetailsModel {
+  AcmId!: number;
+Vdate!: Date ;
+  PartyAcmId: number | null = null;
+  TdsId: number | null = null;
+}
+
+
+
+
+ 
+export class PurchaseList {
+     StatusCode:string= 'null';
+    SearchValue:string= 'null';
+    CmpId:number= 1;
+    BrnId:number= 86;
+    FyrId:number= 25;
+    Vtype:string= '';
+    EmpId:string= 'null';
+    LogId:number= 157325;
+    NavId:number= 181993;
+    MaxRecord:number= 100;
+    PeriodType:string= 'THIS YEAR';
+    SearchColumn:string= '';
+    FromDate:string= 'null';
+    ToDate:string= 'null';
+    DateType:string= 'VDATE';
+    MultiSearchColumns:string= '';
+    BtpId:number= 1;
+    BtpCode:string= 'PO';
+    AllRecord:string= ''
+}
--- src/app/services/vendor-service/vendor-service-file.service.ts
+++ src/app/services/vendor-service/vendor-service-file.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
-import { VendorFormData } from '../../Models/data-structure/vendor.model';
+import { PoFormModel, PurchaseList, SearchVendorModel, TDSDetailsModel, VendorFormData } from '../../Models/data-structure/vendor.model';
 import { Vendor } from '../../Models/interface/Vendor.interface';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { TDSResponse } from '../../pages/vendor/purchase-orders/purchase-orders.component';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,13 +16,114 @@ export class VendorServiceFileService {
 vendors:Vendor[]=[];
 
 
-  constructor() { }
+ 
 
-  addVendor() {
-    this.vendors.push(this.vendor);
-    
-   
-  }
+ poList:any=new PurchaseList();
+  poForm:any= new PoFormModel();
+  poVendor:any= new SearchVendorModel();
+  poTdsDetails:any= new TDSDetailsModel()
+
+
+
+
+  private apiUrl="https://efactoapidevelopment.efacto.cloud/api"
+  private authCode='140-9299-524-TEST'
+  constructor(private http: HttpClient) { }
+
+ getPOList(purchaseList: PurchaseList): Observable<any> {
+  let params = new HttpParams();
+    Object.keys(purchaseList).forEach(key => {
+      const value = (purchaseList as any)[key];
+      if (value !== undefined && value !== null) {
+        params = params.set(key, value.toString());
+      }
+    });
+
+  const headers = new HttpHeaders({
+    'code': this.authCode
+  });
+
+return this.http.get(`${this.apiUrl}/PO/GetPOList`, { headers, params });
+
+}
+
+
+
+  postPoForm(PoFormModel: PoFormModel) {
+  const headers = new HttpHeaders({
+    'Content-Type': 'application/json',
+    'code': this.authCode // Include any custom headers required by the API
+  });
+
+  return this.http.post(`${this.apiUrl}/PO`, PoFormModel, { headers });
+}
+
+getVendor(PoVendorModel: SearchVendorModel) {
+  const headers = new HttpHeaders({
+    'code': this.authCode
+  });
+
+  const params = new HttpParams({ fromObject: { 
+    Search: PoVendorModel.Search,
+    EmpId: PoVendorModel.EmpId?.toString() ?? '',
+    BtpId: PoVendorModel.BtpId.toString(),
+    SearchType: PoVendorModel.SearchType,
+    SearchColumn: PoVendorModel.SearchColumn ?? '',
+    AcgGroups: PoVendorModel.AcgGroups ?? '',
+    AcgCategorys: PoVendorModel.AcgCategorys,
+    MaxRecord: PoVendorModel.MaxRecord.toString(),
+    AcgIds: PoVendorModel.AcgIds,
+    ActIds: PoVendorModel.ActIds.join(',')  // Assuming the API expects CSV
+  }});
+
+  return this.http.get(`${this.apiUrl}/Acm/GetAccountSearchList`, { headers, params });
+}
+
+TDSDetails(AcmId: number) {
+  const params = new HttpParams({
+    fromObject: {
+      AcmId: AcmId.toString(),
+      Vdate: this.poTdsDetails.Vdate,
+      PartyAcmId: this.poTdsDetails.PartyAcmId?.toString() ?? '',
+      TdsId: this.poTdsDetails.TdsId?.toString() ?? ''
+    }
+  });
+ /*  const paramsSearch = new HttpParams({
+    fromObject:{
+      Id:AcmId.toString(),
+      Value:this.poContactSearch.Value,
+      Text1:this.poContactSearch.Text1,
+      Text2:this.poContactSearch.Text2,
+    }
+  }) */
+
+  const headers = new HttpHeaders({
+    'code': this.authCode
+  });
+
+  return this.http.get<TDSResponse>(`${this.apiUrl}/Common/GetTDSDetail`, { headers, params });
+ // 
+}
+
+AcmContactSearchList(id:number){
+   const headers = new HttpHeaders({
+    'code': this.authCode
+  });
+  const params = new HttpParams({ fromObject: {
+  Id:id
+  }})
+  return this.http.get(`${this.apiUrl}/Acm/GetAcmContactSearchList`,{headers,params})
+}
+GetAcmByName(name:string){
+  const headers = new HttpHeaders({
+   'code': this.authCode
+  });
+  const params = new HttpParams({ fromObject: {
+   Name:name
+  }})
+   return this.http.get(`${this.apiUrl}Acm/GetAcmByName`,{headers,params})
+}
+
 
   
   
