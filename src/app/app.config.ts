230e75fdd96d12f9f83d76142fb7286c811ef58f
Gaurav-Nathh <gauravnath725@gmail.com>
2025-09-05 22:03:32 +0530

    Added Hashing Strategy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,11 @@ import {
   ApplicationConfig,
   provideZoneChangeDetection,
 } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { commonRoutes, customerRoute, vendorRoutes } from './app.routes';
 import { ApiConfigService } from './services/api-config/api-config.service';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export function initApp(configService: ApiConfigService) {
   return () => {
@@ -18,6 +19,10 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter([...commonRoutes, ...customerRoute, ...vendorRoutes]),
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy,
+    },
 
     {
       provide: APP_INITIALIZER,
